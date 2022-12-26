5a56773e7a579357348ca58390ca5cf82aa083ba
CF255 <solamente_yo_@outlook.es>
2022-12-26 17:15:01 -0400

    se agrega el proyecto

---
+++ js/js.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+
+    new Glider(document.querySelector('.carousel-list'), {
+        slidesToShow: 7,
+        slidesToScroll: 6,
+        arrows: {
+          prev: '.carousel-previus',
+          next: '.carousel-next'
+        }
+      });
+
+      
+});
